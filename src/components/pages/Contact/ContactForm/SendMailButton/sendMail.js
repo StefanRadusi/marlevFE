0dc8560d2592601af32a307f28b4dc813534e9a9
Gabriel Militaru <gabriel.militaru@aeratechnology.com>
2020-08-07 14:46:53 +0300

    Remove message until they look good

--- src/components/pages/Contact/ContactForm/SendMailButton/sendMail.js
+++ src/components/pages/Contact/ContactForm/SendMailButton/sendMail.js
@@ -4,10 +4,10 @@ export function sendMail(mailData, changeMessage) {
     // For local 'http://localhost:5000/mail'
     axios.post('/mail', mailData)
         .then(function () {
-            changeMessage({text: "Mail-ul a fost trimis cu succes", type: "success"});
+            // changeMessage({text: "Mail-ul a fost trimis cu succes", type: "success"});
         })
         .catch(function (error) {
             console.log('Sending mail failed ', error);
-            changeMessage({text: "Trimiterea mail-ului a esuat", type: "failed"});
+            // changeMessage({text: "Trimiterea mail-ului a esuat", type: "failed"});
         })
 }
