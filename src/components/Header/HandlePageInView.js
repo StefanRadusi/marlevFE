728f458c82037c1836c375a221deb250036b412a
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-09-27 20:34:52 +0300

    make responsive products and calculator

--- src/components/Header/HandlePageInView.js
+++ src/components/Header/HandlePageInView.js
@@ -1,49 +1,51 @@
 import { throttle } from "lodash";
 
 export function handledPageInView(history, location) {
-  const landingPage = document.querySelector(".landing-page");
-  const products = document.querySelector("#produse");
-  const portofolio = document.querySelector("#portofoliu");
-  const contact = document.querySelector("#contact").parentElement
-    .parentElement;
+  if (location.hash) {
+    const landingPage = document.querySelector(".landing-page");
+    const products = document.querySelector("#produse");
+    const portofolio = document.querySelector("#portofoliu");
+    const contact = document.querySelector("#contact").parentElement
+      .parentElement;
 
-  landingPage.addEventListener(
-    "scroll",
-    throttle(() => {
-      if (
-        landingPage.scrollTop >= 0 &&
-        landingPage.scrollTop < products.offsetTop &&
-        location.hash !== "#home"
-      ) {
-        history.push("/#home");
-        return;
-      }
+    landingPage.addEventListener(
+      "scroll",
+      throttle(() => {
+        if (
+          landingPage.scrollTop >= 0 &&
+          landingPage.scrollTop < products.offsetTop &&
+          location.hash !== "#home"
+        ) {
+          history.push("/#home");
+          return;
+        }
 
-      if (
-        landingPage.scrollTop >= products.offsetTop &&
-        landingPage.scrollTop < portofolio.offsetTop &&
-        location.hash !== "#produse"
-      ) {
-        history.push("/#produse");
-        return;
-      }
+        if (
+          landingPage.scrollTop >= products.offsetTop &&
+          landingPage.scrollTop < portofolio.offsetTop &&
+          location.hash !== "#produse"
+        ) {
+          history.push("/#produse");
+          return;
+        }
 
-      if (
-        landingPage.scrollTop >= portofolio.offsetTop &&
-        landingPage.scrollTop < contact.offsetTop &&
-        location.hash !== "#portofoliu"
-      ) {
-        history.push("/#portofoliu");
-        return;
-      }
+        if (
+          landingPage.scrollTop >= portofolio.offsetTop &&
+          landingPage.scrollTop < contact.offsetTop &&
+          location.hash !== "#portofoliu"
+        ) {
+          history.push("/#portofoliu");
+          return;
+        }
 
-      if (
-        landingPage.scrollTop >= contact.offsetTop &&
-        location.hash !== "#contact"
-      ) {
-        history.push("/#contact");
-        return;
-      }
-    }, 500)
-  );
+        if (
+          landingPage.scrollTop >= contact.offsetTop &&
+          location.hash !== "#contact"
+        ) {
+          history.push("/#contact");
+          return;
+        }
+      }, 500)
+    );
+  }
 }
--- src/components/pages/SinglePage/SinglePageDetail.js
+++ src/components/pages/SinglePage/SinglePageDetail.js
@@ -5,59 +5,33 @@ import RemoveIcon from "@material-ui/icons/Remove";
 import { Link } from "react-router-dom";
 
 export function SinglePageDetail(props) {
-  const { detail, index, path } = props;
+  const { detail, path } = props;
 
   return (
     <div className="detail-row">
-      {index % 2 === 1 ? (
-        <>
-          <div className="detail-row__content-container">
-            <h2>{detail.subtitle}</h2>
-            <div className="detail-row__content-description">
-              {detail.description.map((phrase, index) => {
-                return <p key={index}>{phrase}</p>;
-              })}
+      <div className="detail-row__content-container">
+        <h2>{detail.subtitle}</h2>
+        <div className="detail-row__content-description">
+          {detail.description.map((phrase, index) => {
+            return <p key={index}>{phrase}</p>;
+          })}
+        </div>
+        <div className="detail-row_content-calculator">
+          <Link
+            to={`/calculator-oferta/${path}`}
+            className="detail-row_content-calculator"
+          >
+            <div className="detail-row_content-calculator__container">
+              <AddIcon />
+              <RemoveIcon />
             </div>
-            <div className="detail-row_content-calculator">
-              <Link
-                to={`/calculator-oferta/${path}`}
-                className="detail-row_content-calculator"
-              >
-                <div className="detail-row_content-calculator__container">
-                  <AddIcon />
-                  <RemoveIcon />
-                </div>
-                <p>Calculator pret</p>
-              </Link>
-            </div>
-          </div>
-          <img src={detail.image} alt="img"></img>
-        </>
-      ) : (
-        <>
-          <img src={detail.image} alt="img" />
-          <div className="detail-row__content-container">
-            <h2>{detail.subtitle}</h2>
-            <div className="detail-row__content-description">
-              {detail.description.map((phrase, index) => {
-                return <p key={index}>{phrase}</p>;
-              })}
-            </div>
-            <div className="detail-row_content-calculator">
-              <Link
-                to={`/calculator-oferta/${path}`}
-                className="detail-row_content-calculator"
-              >
-                <div className="detail-row_content-calculator__container">
-                  <AddIcon />
-                  <RemoveIcon />
-                </div>
-                <p>Calculator pret</p>
-              </Link>
-            </div>
-          </div>
-        </>
-      )}
+            <p>Calculator oferta</p>
+          </Link>
+        </div>
+      </div>
+      <div className="detail-row__content-container">
+        <img src={detail.image} alt="img"></img>
+      </div>
     </div>
   );
 }
