8dfde4a54ba09df26332e925613afea6008cbd6e
Gabriel Militaru <gabriel.militaru@aeratechnology.com>
2020-09-27 20:55:56 +0300

    Fix oferta

--- src/components/pages/OferteSalvate/ButonTrimiteOferta/trimiteOferta.js
+++ src/components/pages/OferteSalvate/ButonTrimiteOferta/trimiteOferta.js
@@ -4,8 +4,7 @@ export function trimiteOferta(oferta) {
     // For local 'http://localhost:5000/mail'
     const contact = JSON.parse(localStorage.getItem("contactData"));
     contact.oferta = oferta;
-    console.log("data", contact);
-    axios.post('http://localhost:5000/oferta', contact)
+    axios.post('/oferta', contact)
         .then(function () {
             localStorage.removeItem("cart-items");
             window.location.href = "/oferte-salvate/success";
