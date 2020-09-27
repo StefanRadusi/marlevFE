7692e2567e376e5b8c0e9672d3303f6350b16719
Gabriel Militaru <gabriel.militaru@aeratechnology.com>
2020-09-27 21:37:33 +0300

    Fix url

--- src/components/pages/Contact/ContactForm/SendMailButton/sendMail.js
+++ src/components/pages/Contact/ContactForm/SendMailButton/sendMail.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function sendMail(mailData, changeMessage) {
     // For local 'http://localhost:5000/mail'
-    axios.post('/mail', mailData)
+    axios.post('http://marlevv2-env1.eba-zdyw58pm.us-east-2.elasticbeanstalk.com/mail', mailData)
         .then(function () {
             // changeMessage({text: "Mail-ul a fost trimis cu succes", type: "success"});
         })
--- src/components/pages/OferteSalvate/ButonTrimiteOferta/trimiteOferta.js
+++ src/components/pages/OferteSalvate/ButonTrimiteOferta/trimiteOferta.js
@@ -4,7 +4,7 @@ export function trimiteOferta(oferta) {
     // For local 'http://localhost:5000/mail'
     const contact = JSON.parse(localStorage.getItem("contactData"));
     contact.oferta = oferta;
-    axios.post('/oferta', contact)
+    axios.post('http://marlevv2-env1.eba-zdyw58pm.us-east-2.elasticbeanstalk.com/oferta', contact)
         .then(function () {
             localStorage.removeItem("cart-items");
             window.location.href = "/oferte-salvate/success";
